2f07b218f93a727ac397594f817e450765272983
sumeet tiwari <sumeet.tiwari@tlkn.com>
2018-02-06 15:50:19 +0530

    Update twitter_rest.erl

--- src/twitter_rest.erl
+++ src/twitter_rest.erl
@@ -38,7 +38,7 @@ post(#twitter{auth=#oauth{token=Token} = Auth,
     BaseUrl = make_stream_url(Twitter, "statuses/filter", ""),
     Request = twitter_auth:make_post_request(Auth, BaseUrl, Args),
     {ok, Body} = request(post_stream, Request),
-    spawn(handle_connection(Callback, Body)),
+    handle_connection(Callback, Body),
     {ok, Body}.
 
 post(#twitter{auth=#oauth{token=Token} = Auth,
